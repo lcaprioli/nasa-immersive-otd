d2c685737fd33b322781ba0be61c7cfc8bc701eb
luizcaprioli <lcaprioli@gmail.com>
2023-11-12 18:34:11 -0300

    Timeline Bloc State UPDATED

--- lib/features/immersive/presentation/timeline/bloc/timeline_state.dart
+++ lib/features/immersive/presentation/timeline/bloc/timeline_state.dart
@@ -2,36 +2,50 @@ import 'package:equatable/equatable.dart';
 import 'package:nasa_immersive_od/features/immersive/domain/entities/immersive_entity.dart';
 
 abstract class TimelineState extends Equatable {
-  const TimelineState({required this.immersives});
+  const TimelineState({
+    required this.immersives,
+    required this.page,
+  });
   final Set<ImmersiveEntity> immersives;
+  final int page;
 
   @override
   List<Object?> get props => [immersives];
 }
 
 class TimelineInitial extends TimelineState {
-  const TimelineInitial() : super(immersives: const {});
+  const TimelineInitial()
+      : super(
+          immersives: const {},
+          page: 0,
+        );
 
   @override
   List<Object?> get props => [immersives];
 }
 
 class TimelineInProgress extends TimelineState {
-  const TimelineInProgress() : super(immersives: const {});
+  const TimelineInProgress({required super.page}) : super(immersives: const {});
 
   @override
   List<Object?> get props => [immersives];
 }
 
 class TimelineInSuccess extends TimelineState {
-  const TimelineInSuccess({required super.immersives});
+  const TimelineInSuccess({
+    required super.immersives,
+    required super.page,
+  });
 
   @override
   List<Object?> get props => [immersives];
 }
 
 class TimelineError extends TimelineState {
-  const TimelineError(this.error) : super(immersives: const {});
+  const TimelineError({
+    required super.page,
+    required this.error,
+  }) : super(immersives: const {});
 
   final Exception error;
   @override
