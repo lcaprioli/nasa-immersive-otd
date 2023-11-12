bbd1a66160d8dfc98bc19d48fe941ac886771247
luizcaprioli <lcaprioli@gmail.com>
2023-11-12 12:35:07 -0300

    Timeline Event CREATED

---
+++ lib/features/immersive/presentation/timeline/bloc/timeline_event.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+abstract class TimelineEvent extends Equatable {
+  const TimelineEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class TimelineStarted extends TimelineEvent {
+  const TimelineStarted();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class TimelinePageChanged extends TimelineEvent {
+  const TimelinePageChanged(this.page);
+
+  final int page;
+  @override
+  List<Object?> get props => [];
+}
