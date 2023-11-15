84d1c1d8aa516ea45654934ddd1ab49ddd88a5cb
luizcaprioli <lcaprioli@gmail.com>
2023-11-15 11:30:10 -0300

    Timeline Page Responsiveness DONE

--- lib/features/immersive/presentation/timeline/widgets/timeline_page.dart
+++ lib/features/immersive/presentation/timeline/widgets/timeline_page.dart
@@ -1,6 +1,9 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
+import 'package:nasa_immersive_od/features/immersive/domain/entities/immersive_entity.dart';
 import 'package:nasa_immersive_od/features/immersive/presentation/timeline/bloc/timeline_bloc.dart';
 import 'package:nasa_immersive_od/features/immersive/presentation/timeline/bloc/timeline_state.dart';
 
@@ -23,109 +26,202 @@ class _TimelinePageState extends State<TimelinePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocConsumer<TimelineBloc, TimelineState>(
-          bloc: widget.bloc,
-          listener: (context, state) {
-            if (state is TimelineError) {
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text(state.message)));
-            }
-          },
-          builder: (context, state) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 40,
-                vertical: 20,
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                'Immersive of the Day',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineMedium
+                    ?.copyWith(color: Colors.white),
+                textAlign: TextAlign.center,
               ),
-              child: switch (state) {
-                TimelineInitial() => const SizedBox.shrink(),
-                TimelineInProgress() => const CircularProgressIndicator(),
-                TimelineSuccess() => Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: PageView.builder(
-                          itemCount: state.immersives.length,
-                          itemBuilder: (_, index) => Text(
-                            state.immersives.elementAt(index).title,
-                            textAlign: TextAlign.center,
+            ),
+            Expanded(
+              child: BlocConsumer<TimelineBloc, TimelineState>(
+                  bloc: widget.bloc,
+                  listener: (context, state) {
+                    if (state is TimelineError) {
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text(state.message)));
+                    }
+                  },
+                  builder: (context, state) {
+                    return Center(
+                      child: switch (state) {
+                        TimelineInitial() => const SizedBox.shrink(),
+                        TimelineInProgress() =>
+                          const CircularProgressIndicator(),
+                        TimelineSuccess() => Column(
+                            children: [
+                              Flexible(
+                                flex: 3,
+                                child: TimelineCarousel(
+                                  immersives: state.immersives,
+                                ),
+                              ),
+                              ColoredBox(
+                                color: Colors.grey,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(20),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        child: FilledButton(
+                                          onPressed: widget.bloc.prev,
+                                          child: const Text('< Previous'),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: 20,
+                                      ),
+                                      Expanded(
+                                        child: FilledButton(
+                                          onPressed: state.page > 0
+                                              ? widget.bloc.next
+                                              : null,
+                                          child: const Text('Next >'),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FilledButton(
-                            onPressed: widget.bloc.prev,
-                            child: const Text('<'),
-                          ),
-                          const SizedBox(
-                            width: 15,
-                          ),
-                          FilledButton(
-                            onPressed: state.page > 0 ? widget.bloc.next : null,
-                            child: const Text('>'),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: state.immersives
-                            .map(
-                              (e) => Text(DateFormat.MMMd().format(e.date)),
-                            )
-                            .toList(),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FilledButton(
-                            onPressed: widget.bloc.prev,
-                            child: const Text('<'),
-                          ),
-                          const SizedBox(
-                            width: 15,
-                          ),
-                          FilledButton(
-                            onPressed: state.page > 0 ? widget.bloc.next : null,
-                            child: const Text('>'),
-                          ),
-                        ],
-                      ),
-                    ],
+                        TimelineError() => Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Icon(
+                                Icons.crisis_alert,
+                                size: 95,
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                state.message,
+                                textAlign: TextAlign.center,
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              FilledButton(
+                                onPressed: widget.bloc.refresh,
+                                child: const Text('Retry'),
+                              )
+                            ],
+                          )
+                      },
+                    );
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TimelineCarousel extends StatefulWidget {
+  const TimelineCarousel({
+    super.key,
+    required this.immersives,
+  });
+
+  final Set<ImmersiveEntity> immersives;
+
+  @override
+  State<TimelineCarousel> createState() => _TimelineCarouselState();
+}
+
+class _TimelineCarouselState extends State<TimelineCarousel> {
+  late PageController _controller;
+  late int _actual;
+  @override
+  void initState() {
+    _actual = widget.immersives.length - 1;
+    _controller = PageController(initialPage: _actual);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant TimelineCarousel oldWidget) {
+    _actual = widget.immersives.length - 1;
+    _controller = PageController(initialPage: _actual);
+
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Flexible(
+            flex: 5,
+            child: PageView.builder(
+              controller: _controller,
+              itemCount: widget.immersives.length,
+              itemBuilder: (_, index) => Column(
+                children: [
+                  Expanded(
+                    child: Image.memory(
+                      Uint8List.fromList(
+                          widget.immersives.elementAt(index).imageBytes ?? []),
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                TimelineError() => Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Icon(
-                        Icons.crisis_alert,
-                        size: 95,
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Text(
-                        state.message,
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      FilledButton(
-                        onPressed: widget.bloc.refresh,
-                        child: const Text('Retry'),
-                      )
-                    ],
-                  )
-              },
-            );
-          }),
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Text(
+                      widget.immersives.elementAt(index).title,
+                      style: Theme.of(context).textTheme.headlineSmall,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: List.generate(
+                widget.immersives.length,
+                (index) => Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: FilledButton(
+                      style: index == _actual
+                          ? FilledButton.styleFrom(
+                              backgroundColor: Colors.deepOrangeAccent)
+                          : null,
+                      onPressed: () => setState(() {
+                        _actual = index;
+                        _controller.jumpToPage(index);
+                      }),
+                      child: Text(DateFormat.MMMd()
+                          .format(widget.immersives.elementAt(index).date)),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
