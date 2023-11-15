da6db6d4b4065ab723177789091f43f250bc8dea
luizcaprioli <lcaprioli@gmail.com>
2023-11-15 18:52:35 -0300

    Custom Exceptions CREATED

---
+++ lib/features/immersive/domain/exceptions/exceptions.dart
@@ -0,0 +1,9 @@
+class ServerException implements Exception {}
+
+class LocalStorageException implements Exception {}
+
+class NoRemoteDataException implements Exception {}
+
+class NoLocalDataException implements Exception {}
+
+class ImageDownloadException implements Exception {}
